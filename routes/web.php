f3ca6eec2f2fa96c5555b6faa525dcdccf54219e
colidom <colidom@outlook.com>
2023-11-05 22:47:42 +0100

    Routing en laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('principal');
+});
+
+Route::get('/nosotros', function () {
+    return view('nosotros');
+});
+Route::get('/tienda', function () {
+    return view('tienda');
 });
