a2da30003c57d31cf6859d9876abcfa30783194a
colidom <colidom@outlook.com>
2024-02-02 16:40:19 +0100

    Creando el Formulario de Registro en Devstagram

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,3 +3,48 @@
 @section('titulo')
 Regístrate en DevStagram
 @endsection
+
+@section('contenido')
+<div class="md:flex md:justify-center md:gap-10 md:items-center">
+    <div class="md:w-6/12 p-5">
+        <img src="{{ asset('img/registrar.jpg') }}" alt="Imagen registro usuarios">
+    </div>
+
+    <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+        <form method="POST" action="">
+            <div class="mb-5">
+                <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                    Nombre
+                </label>
+                <input id="name" name="name" type="text" placeholder="Tu nombre" class="border p-3 w-full rounded-lg">
+            </div>
+            <div class="mb-5">
+                <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                    Nombre de usuario
+                </label>
+                <input id="username" name="username" type="text" placeholder="Tu nombre de usuario" class="border p-3 w-full rounded-lg">
+            </div>
+            <div class="mb-5">
+                <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                    Email
+                </label>
+                <input id="email" name="email" type="text" placeholder="Tu email de registro" class="border p-3 w-full rounded-lg">
+            </div>
+            <div class="mb-5">
+                <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                    Contraseña
+                </label>
+                <input id="password" name="password" type="password" placeholder="Tu contraseña de registro" class="border p-3 w-full rounded-lg">
+            </div>
+            <div class="mb-5">
+                <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
+                    Repetir Contraseña
+                </label>
+                <input id="password_confirmation" name="password_confirmation" type="password" placeholder="Tu contraseña de registro" class="border p-3 w-full rounded-lg">
+            </div>
+
+            <input type="submit" value="Crear cuenta" class="bg-sky-600 hover:bg-sky-700 cursor-progress uppercase font-bold w-full  '-3 text-white rounded-lg">
+        </form>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
         <h2 class="font-black text-center text-3xl mb-10">@yield('titulo')</h2>
         @yield('contenido')
     </main>
-    <footer class="text-center p-5 text-gray-500 font-bold">
+    <footer class=" mt-10 text-center p-5 text-gray-500 font-bold">
         DevStagram · Todos los derechos reservados · {{now()->year}}
     </footer>
 </body>
