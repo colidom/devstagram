229c3af91a24ade1cf8cd2848c43ff7e92fa35e8
colidom <colidom@outlook.com>
2024-02-02 01:38:43 +0100

    Instalando Tailwind CSS con Vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,16 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Devstagram - @yield('titulo')</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <nav>
         <a href="/">Principal</a>
         <a href="/nosotros">Nosotros</a>
         <a href="/tienda">Tienda</a>
-        <a href="/contacto">Contacto</a>
     </nav>
 
-    <h1>@yield('titulo')</h1>
+    <h1 class="text-4xl text-extrabold">@yield('titulo')</h1>
 
     <hr>
 
