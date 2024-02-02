49f47b5a33ac747bafcd659e1530f1b4bfbcf491
colidom <colidom@outlook.com>
2024-02-02 17:22:26 +0100

    Implementado controlador de home

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('principal');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 // Login
 Route::get('/login', [LoginController::class, 'index']);
