fb56912aa473375119def923302e600c9d385ace
colidom <colidom@outlook.com>
2024-02-03 22:22:39 +0100

    Mostrar el nombre del usuario

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,8 @@ class PostController extends Controller
 
     public function index(User $user)
     {
-        return view('dashboard');
+        return view('dashboard', [
+            'user' => $user
+        ]);
     }
 }
