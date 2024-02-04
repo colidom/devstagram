c66cf2524058dc08d740fe80783df5cac6c66071
colidom <colidom@outlook.com>
2024-02-04 23:58:07 +0100

    Finalizando el Formulario

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,18 +13,32 @@
             <form method="POST" action="{{ route('register') }}">
                 @csrf
                 <div class="mb-5">
-                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
-                        Nombre
+                    <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Titulo
                     </label>
-                    <input id="name" name="name" type="text" placeholder="Tu nombre"
-                        class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
-                        value="{{ old('name') }}" />
-                    @error('name')
+                    <input id="titulo" name="titulo" type="text" placeholder="Titulo de la publicación"
+                        class="border p-3 w-full rounded-lg @error('titulo') border-red-500 @enderror"
+                        value="{{ old('titulo') }}" />
+                    @error('titulo')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                             {{ $message }}
                         </p>
                     @enderror
                 </div>
+                <div class="mb-5">
+                    <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Descripción
+                    </label>
+                    <textarea id="descripcion" name="descripcion" placeholder="Descripción de la publicación"
+                        class="border p-3 w-full rounded-lg @error('titulo') border-red-500 @enderror">{{ old('titulo') }}</textarea>
+                    @error('titulo')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <input type="submit" value="Crear publicación"
+                    class="bg-sky-600 hover:bg-sky-700 cursor-pointer uppercase font-bold w-full '-3 text-white rounded-lg" />
             </form>
         </div>
     @endsection
