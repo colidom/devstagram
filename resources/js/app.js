96a2ab4eed112535c1971bbf580ba24e04c45c09
colidom <colidom@outlook.com>
2024-02-08 16:47:53 +0100

    Corregido problema de dropzone
    Se corrige problema de dropzone en todas las vistas ya que daba error "Uncaught Error: Invalid dropzone element" porque se carga el js por todas las vistas pero el objeto dropzone solo necesitamos crearlo en la vista de posts/create. Ahora solo se creará el objeto dropzone cuando exista el elemento #dropzone dentro de un frmulario

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,25 +2,27 @@ import Dropzone from "dropzone";
 
 Dropzone.autoDiscover = false;
 
-const dropzone = new Dropzone("#dropzone", {
-    dictDefaultMessage: "Sube aquí tu imagen",
-    acceptedFiles: ".png, .jpg, .jpeg, .gif",
-    addRemoveLinks: true,
-    dictRemoveFile: "Borrar archivo",
-    maxFiles: 1,
-    uploadMultiple: false,
-});
+if (document.querySelector("#dropzone")) {
+    const dropzone = new Dropzone("#dropzone", {
+        dictDefaultMessage: "Sube aquí tu imagen",
+        acceptedFiles: ".png, .jpg, .jpeg, .gif",
+        addRemoveLinks: true,
+        dictRemoveFile: "Borrar archivo",
+        maxFiles: 1,
+        uploadMultiple: false,
+    });
 
-dropzone.on("sending", function (file, xhr, formData) {
-    console.log(file);
-});
+    dropzone.on("sending", function (file, xhr, formData) {
+        console.log(file);
+    });
 
-dropzone.on("success", function (file, response) {
-    console.log(response);
-});
-dropzone.on("error", function (file, message) {
-    console.log(message);
-});
-dropzone.on("removedfile", function () {
-    console.log("Archivo eliminado...");
-});
+    dropzone.on("success", function (file, response) {
+        console.log(response);
+    });
+    dropzone.on("error", function (file, message) {
+        console.log(message);
+    });
+    dropzone.on("removedfile", function () {
+        console.log("Archivo eliminado...");
+    });
+}
