710ee53bb530838e26d76ae2666eca214b53457b
colidom <colidom@outlook.com>
2024-02-12 16:15:35 +0100

    Almacenando Publicaciones

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /* Info que se llena en DB para que laravel sepa que
+    procesar antes de enviar a DB, siempre será la misma que
+    en el controller al llamar el método Post::create().
+    */
+    protected $fillable = [
+        'titulo',
+        'descripcion',
+        'imagen',
+        'user_id'
+    ];
 }
