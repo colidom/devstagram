fdb12576a924ed12996fb695dc79b6f8331bfd38
colidom <colidom@outlook.com>
2024-02-18 20:12:18 +0100

    Añadida clase

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -32,16 +32,18 @@
 
                     <form action="{{ route('posts.show', ['post' => $post, 'user' => $user]) }}" method="POST">
                         @csrf
-                        <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">
-                            Añade un comentario
-                        </label>
-                        <textarea id="comentario" name="comentario" placeholder="Comentario"
-                            class="border p-3 w-full rounded-lg @error('comentario') border-red-500 @enderror"></textarea>
-                        @error('comentario')
-                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
-                                {{ $message }}
-                            </p>
-                        @enderror
+                        <div class="mb-5">
+                            <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">
+                                Añade un comentario
+                            </label>
+                            <textarea id="comentario" name="comentario" placeholder="Comentario"
+                                class="border p-3 w-full rounded-lg @error('comentario') border-red-500 @enderror"></textarea>
+                            @error('comentario')
+                                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                                    {{ $message }}
+                                </p>
+                            @enderror
+                        </div>
                         <input type="submit" value="Comentar"
                             class="bg-sky-600 hover:bg-sky-700 cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg" />
                     </form>
