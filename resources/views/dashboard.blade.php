6369ab31a9bab96a5a7ffaaca344e97c3a13829d
colidom <colidom@outlook.com>
2024-02-21 23:13:03 +0100

    Ajustes en las clases de las publicaciones

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,11 +30,11 @@
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
 
         @if ($userPosts->count())
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <div class="grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-1">
                 @foreach ($userPosts as $post)
                     <div>
                         <a href="{{ route('posts.show', ['post' => $post, 'user' => $user]) }}">
-                            <img src="{{ asset('uploads') . '/' . $post->imagen }}"
+                            <img class="w-full " src="{{ asset('uploads') . '/' . $post->imagen }}"
                                 alt="Imagen del post {{ $post->titulo }}">
                         </a>
                     </div>
