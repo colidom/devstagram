8bffe87b6bf80bf61eeb7299a8da90eb0461d95d
colidom <colidom@outlook.com>
2024-02-25 22:26:09 +0100

    Obtener los posts de los usuarios que seguimos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    // Definir la constante de paginación
+    const ELEMENTS_BY_PAGE = 20;
+
     // Usamos __invoke si el controller solo va a tener un método
     public function __invoke()
 
     {
         // Obtener los usuarios que seguimos
-        dd(auth()->user()->followings->pluck('id')->toArray());
-        return view('home');
+        $idsFollowins = auth()->user()->followings->pluck('id')->toArray();
+        $posts = Post::whereIn('user_id', $idsFollowins)->paginate(self::ELEMENTS_BY_PAGE);
+
+        return view('home', [
+            'posts', $posts
+        ]);
     }
 }
