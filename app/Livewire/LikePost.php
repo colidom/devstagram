eb242cf6cb161d8c258ab4f6b2bc72d5fadeefa6
colidom <colidom@outlook.com>
2024-02-26 22:57:45 +0100

    Actualizar el Contador de Likes

--- app/Livewire/LikePost.php
+++ app/Livewire/LikePost.php
@@ -8,10 +8,12 @@ class LikePost extends Component
 {
     public $post;
     public $isLiked;
+    public $likes;
 
     public function mount($post)
     {
         $this->isLiked = $post->checkLike(auth()->user());
+        $this->likes = $post->likes->count();
     }
 
     public function like()
@@ -20,12 +22,14 @@ class LikePost extends Component
             // Unlike the post
             $this->post->likes()->where('post_id', $this->post->id)->delete();
             $this->isLiked = false;
+            $this->likes--;
         } else {
             // Like the post
             $this->post->likes()->create([
                 'user_id' => auth()->user()->id
             ]);
             $this->isLiked = true;
+            $this->likes++;
         }
     }
 
